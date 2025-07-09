49032b11ab2c7ea389c52401ac101459a0c98701
jp88-cyn <priscilaibarguen83@gmail.com>
2025-07-09 15:48:37 -0300

    Commit para Hospedagem

---
+++ Projeto_Final/songs.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    src: "./songs/full-moon.mp3",
+    name: "Full Moon Full Life - P3R",
+  },
+  {
+    src: "./songs/reaching-stars.mp3",
+    name: " When The Moon's Reaching Out Stars - P3R Soundtrack ",
+  },
+  
+];
